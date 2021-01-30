4b73d60a2ec7640286a127e79756efd9eb717105
Harshit Rai <harshitrai68@gmail.com>
2021-01-30 14:05:17 +0530

    Changed default values for arguments

--- starter_file/train.py
+++ starter_file/train.py
@@ -1,16 +1,17 @@
 from sklearn.ensemble import RandomForestClassifier
 import argparse
 import os
-from sklearn.metrics import accuracy
 import joblib
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import numpy as np
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
 ds = TabularDatasetFactory.from_delimited_files(path="https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data",header=False)
 
 def clean_data(raw_df):
+    raw_df = raw_df.to_pandas_dataframe().dropna()
     raw_x = raw_df[raw_df.columns[:-1]]
     raw_y = raw_df[raw_df.columns[-1]]
     labels_dict = {"unacc": 0, "acc":1, "good":2, "vgood":4}
@@ -23,9 +24,9 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--n_estimators', type=int, default=100, help="The number of trees in the forest")
-    parser.add_argument('--max_depth', type=int, default=None, help="Maximum depth of the tree")
+    parser.add_argument('--max_depth', type=int, default=10, help="Maximum depth of the tree")
     parser.add_argument('--criterion', type=str, default="gini", help="Function to measure the quality of the split")
-    parser.add_argument('--max_features', type=int, default="auto", help="Number of features to consider when looking for the best split")
+    parser.add_argument('--max_features', type=int, default=5, help="Number of features to consider when looking for the best split")
 
     args = parser.parse_args()
     
@@ -35,10 +36,10 @@ def main():
     model = RandomForestClassifier(n_estimators=args.n_estimators,max_depth=args.max_depth,criterion=args.criterion,max_features=args.max_features).fit(x_train, y_train)
     
     run = Run.get_context()
-    run.log("Number of Estimators: ", np.float(args.n_estimators))
-    run.log("Maximum Depth: ", np.float(args.max_depth))
-    run.log("Criterion: ", args.criterion)
-    run.log("Maximum Features: ", np.float(args.max_features))
+    run.log("Number of Estimators", np.int(args.n_estimators))
+    run.log("Maximum Depth", np.int(args.max_depth))
+    run.log("Criterion", args.criterion)
+    run.log("Maximum Features", np.int(args.max_features))
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
